fa8bef92ee1ae88c7d0b65e22e09c483a0b01388
Fulei Nataliya <fulei.nataliya@gmail.com>
2019-07-25 21:55:50 +0300

    fix commit Maks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,37 +5,26 @@ import main.java.exception.MoreThan100Exceptions;
 import main.java.exception.NullExceptions;
 import main.java.exception.PalindromeExceptions;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the word: ");
-        Scanner scanner = new Scanner(System.in);
-        String obj = scanner.nextLine();
-        String o = "";
-        if (obj.equals("")||obj.equals(" ")) {
-            o = null;
-        }
-        System.out.println("Enter the number: ");
-        Integer i = Integer.parseInt(scanner.nextLine());
-        scanner.close();
+
         try {
-            Server.palindrome(obj);
+            Server.palindrome();
         } catch (PalindromeExceptions e) {
             e.printStackTrace();
         }
         try {
-            Server.oddNumber(i);
+            Server.isOddNumber();
         } catch (IntOddException e) {
             e.printStackTrace();
         }
         try {
-            Server.number(i);
+            Server.checkNumber();
         } catch (MoreThan100Exceptions e) {
             e.printStackTrace();
         }
         try {
-            Server.object(o);
+            Server.checkNullObject();
         } catch (NullExceptions e) {
             e.printStackTrace();
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,11 +6,15 @@ import main.java.exception.NullExceptions;
 import main.java.exception.PalindromeExceptions;
 
 import java.util.Optional;
+import java.util.Scanner;
 
+class Server {
+    private static Scanner scanner = new Scanner(System.in);
 
-public class Server {
+    static void palindrome() throws PalindromeExceptions {
+        System.out.println("Enter the word: ");
+        String p = scanner.nextLine();
 
-    static void palindrome(String p) throws PalindromeExceptions {
         if (p.equalsIgnoreCase(new StringBuilder(p).reverse().toString())) {
             System.out.println("It is palindrome");
         } else {
@@ -18,22 +22,41 @@ public class Server {
         }
     }
 
-    static void oddNumber(Integer n) throws IntOddException {
+    static void isOddNumber() throws IntOddException {
+        System.out.println("Enter the integer: ");
+        int n = 0;
+        try {
+            n = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
         if (n % 2 == 0) {
-            System.out.println("It is number");
+            System.out.println("It is odd number");
         } else {
-            throw new IntOddException("It is not number");
+            throw new IntOddException("It is not odd number");
         }
     }
 
-    static void number(Integer a) throws MoreThan100Exceptions {
+    static void checkNumber() throws MoreThan100Exceptions {
+        System.out.println("Enter the integer: ");
+        int a = 0;
+        try {
+            a = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
         if (a > 100) {
             throw new MoreThan100Exceptions("More 100");
         }
     }
 
-    static void object(String o) throws NullExceptions {
-        Optional<String> optional = Optional.ofNullable(o);
+    static void checkNullObject() throws NullExceptions {
+        System.out.println("Enter the word: ");
+        String obj = scanner.nextLine();
+        if (obj.equals("") || obj.equals(" ")) {
+            obj = null;
+        }
+        Optional<String> optional = Optional.ofNullable(obj);
         System.out.println(" This is optional " + optional.orElseThrow(NullExceptions::new));
     }
 }
